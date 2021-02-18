37940b619c310e23c88f07813a312803c2a12bb4
jo185040 <Jeffrey.Oyeneye@ncr.com>
2021-02-18 15:07:07 -0600

    Rename project to AmobDroidAutomation and some mods by Dmitri

---
+++ AppiumSpecFlowDemo/Hooks/InitializeHook.cs
@@ -0,0 +1,30 @@
+﻿using AppiumSpecFlowDemo.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AppiumSpecFlowDemo.Hooks
+{
+
+    [Binding]
+    class InitializeHook
+    {
+
+
+        private readonly ScenarioContext _scenarioContext;
+
+        public InitializeHook(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [BeforeScenario]
+        public void Initialize()
+        {
+            AppiumDriver appiumDriver = new AppiumDriver();
+            //context injection sets the type 
+            _scenarioContext.Set(appiumDriver.InitializeAppium());
+        }
+    }
+}
